13ce04d4c977e72739d4c1a0ae4037019fb87a64
Tomas Ospina Gaviria <tospinag@unal.edu.co>
2025-02-22 16:13:21 -0500

    Empleado y Tienda: update

---
+++ gestorAplicacion/gestion/Empleado.py
@@ -0,0 +1,77 @@
+from datetime import date, timedelta
+from typing import List
+
+class Empleado:
+    class Especialidad:
+        VETERINARIO = "VETERINARIO"
+        PELUQUERO = "PELUQUERO"
+        ENTRENADOR = "ENTRENADOR"
+        VENDEDOR = "VENDEDOR"
+    
+    def __init__(self, nombre: str, edad: int, cedula: int, telefono: int, direccion: str, profesion: str):
+        self.nombre = nombre
+        self.edad = edad
+        self.cedula = cedula
+        self.telefono = telefono
+        self.direccion = direccion
+        self.profesion = profesion
+        self.mascota = None
+        self.agenda_dias: List[List[Cupo]] = []
+        
+        if profesion != Empleado.Especialidad.VENDEDOR:  # El vendedor no tiene citas.
+            self.llenar_agenda()
+    
+    def llenar_agenda(self):
+        ciclos = [6, 5, 4, 3, 2, 1]
+        fecha_actual = date.today()
+        dia_semana = fecha_actual.weekday()
+        
+        if dia_semana == 6:  # Si es domingo, se llenan de lunes a sábado
+            fecha_actual += timedelta(days=1)
+            for _ in range(6):
+                self.agenda_dias.append(self.crear_cupos_dia(fecha_actual))
+                fecha_actual += timedelta(days=1)
+        else:
+            for _ in range(ciclos[dia_semana]):
+                self.agenda_dias.append(self.crear_cupos_dia(fecha_actual))
+                fecha_actual += timedelta(days=1)
+            
+            if dia_semana != 0 and dia_semana != 6:  # Se excluye el domingo
+                fecha_actual += timedelta(days=1)
+                for _ in range(dia_semana):
+                    self.agenda_dias.append(self.crear_cupos_dia(fecha_actual))
+                    fecha_actual += timedelta(days=1)
+    
+    @staticmethod
+    def crear_cupos_dia(fecha):
+        return [
+            Cupo(fecha, "8:00", "10:00", True),
+            Cupo(fecha, "10:00", "12:00", True),
+            Cupo(fecha, "14:00", "16:00", True),
+            Cupo(fecha, "16:00", "18:00", True)
+        ]
+    
+    def actualizar_datos(self):
+        for cupos_dia in self.agenda_dias:
+            Cupo.actualizar_cupo(cupos_dia)
+    
+    def tiene_cupos(self) -> bool:
+        self.actualizar_datos()
+        return any(cupo.is_disponible() for cupos_dia in self.agenda_dias for cupo in cupos_dia)
+    
+    def cupos_disponibles(self, dia: int) -> List[Cupo]:
+        disponibles = []
+        for cupos_dia in self.agenda_dias:
+            if cupos_dia[0].get_dia().weekday() == dia:
+                disponibles.extend(cupo for cupo in cupos_dia if cupo.is_disponible())
+                break
+        return disponibles
+    
+    def set_mascota(self, mascota):
+        self.mascota = mascota
+    
+    def get_mascota(self):
+        return self.mascota
+    
+    def __str__(self):
+        return f"Nombre: {self.nombre}, Edad: {self.edad} años."
---
+++ gestorAplicacion/gestion/Tienda.py
@@ -0,0 +1,108 @@
+from gestorAplicacion.elementos import CentroAdopcion, Cliente, Producto
+
+class Tienda:
+    # Listas estáticas para la serialización
+    productos = []
+    empleados = []
+
+    def __init__(self, empleado, centro_adopcion=None):
+        Tienda.empleados.append(empleado)
+        self.centro_adopcion = centro_adopcion
+
+    # Getters y Setters
+    def set_centro_adopcion(self, centro_adopcion):
+        self.centro_adopcion = centro_adopcion
+
+    def get_centro_adopcion(self):
+        return self.centro_adopcion
+
+    @staticmethod
+    def get_productos():
+        return Tienda.productos
+
+    @staticmethod
+    def get_empleados():
+        return Tienda.empleados
+
+    def set_productos(self, productos):
+        Tienda.productos = productos
+
+    def set_empleados(self, empleados):
+        Tienda.empleados = empleados
+
+    # Métodos
+    def agregar_empleado(self, empleado):
+        Tienda.empleados.append(empleado)
+
+    def empleados_tienda(self):
+        return Tienda.empleados
+
+    @staticmethod
+    def agregar_producto(producto):
+        if Tienda.empleados:
+            Tienda.productos.append(producto)
+
+    def inventario(self):
+        if Tienda.empleados:
+            resultado = ""
+            for i, producto in enumerate(Tienda.productos, start=1):
+                resultado += f"{i}. {producto}\n"
+            return resultado
+        else:
+            return "🚫Lo lamentamos pero el centro UNamascota no tiene empleados disponibles para atender su solicitud.🚫"
+
+    def filtrar(self, tipo):
+        if Tienda.empleados:
+            resultado = ""
+            for i, producto in enumerate(Tienda.productos, start=1):
+                if producto.get_tipo_animal() == tipo or producto.get_tipo_animal() == "Uso general":
+                    resultado += f"{i}. {producto}\n"
+            return resultado
+        else:
+            return "🚫Lo lamentamos pero el centro UNamascota no tiene empleados disponibles para atender su solicitud.🚫"
+
+    def compra(self, indice, cliente, unidades=1):
+        if Tienda.empleados:
+            indice -= 1
+            if indice >= len(Tienda.productos) or indice < 0:
+                return "🚫¡Oh no!, verifica que el índice del producto sea el indicado, el centro UNamascota desconoce ese índice.🚫"
+
+            producto = Tienda.productos[indice]
+            cantidad = producto.get_cantidad_unidades()
+
+            if cantidad > 0 and cantidad >= unidades:
+                cantidad -= unidades
+                producto.set_cantidad_unidades(cantidad)
+
+                nombre = producto.get_nombre()
+                tipo = producto.get_tipo_animal()
+                precio = producto.get_precio() * unidades
+
+                if cantidad == 0:
+                    Tienda.productos.pop(indice)
+
+                cliente = CentroAdopcion.es_cliente(cliente)
+                puntos = cliente.get_puntos()
+
+                if puntos >= 15:
+                    cliente.disminuir_puntos(15)
+                    precio *= 0.9  # Aplicar 10% de descuento
+                    return (f"-------------------------------------------\n"
+                            f"Has comprado {unidades} unidades de: {nombre} - Dirigido a: {tipo}\n"
+                            f"Total a pagar: {precio} $\n"
+                            f"Han sido descontados 15 puntos para un 10% de descuento\n"
+                            f"Puntos restantes en tu cartera de puntos: {cliente.get_puntos()}\n"
+                            f"-------------------------------------------")
+                else:
+                    return (f"-------------------------------------------\n"
+                            f"Has comprado {unidades} unidades de: {nombre} - Dirigido a: {tipo}\n"
+                            f"Total a pagar: {precio} $\n"
+                            f"No cuentas con los suficientes puntos como para un descuento\n"
+                            f"-------------------------------------------")
+            else:
+                return "🚫¡Oh no!, verifica la cantidad de unidades disponibles en la tienda y vuelve a intentarlo.🚫"
+        else:
+            return "🚫Lo lamentamos pero el centro UNamascota no tiene empleados disponibles para atender su solicitud.🚫"
+
+    def __str__(self):
+        return f"Tienda con empleados: {self.empleados_tienda()}"
